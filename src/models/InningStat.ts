74359d297937695d28cbac3ef08972c5122b7738
sandeep bhaskar <sandeep.bhaskar@us.gt.com>
2023-02-13 18:42:11 +0530

    added configurations for api project.

---
+++ src/models/InningStat.ts
@@ -0,0 +1,16 @@
+export class InningStat{
+    teamName: string
+    runsScored: number
+    wicketsTaken: number
+    oversPlayed: number
+    isFirstInning: boolean
+    extras: Extras
+
+}
+
+class Extras{
+    wide: number
+    noBall: number
+    bye: number
+    legBye: number
+}
